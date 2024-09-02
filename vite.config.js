383fb3eccb974218aacd86becd2aeb1e7141ed67
tanishq <gaurtanishq07@gmail.com>
2024-09-02 15:21:37 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,  // use the environment variable PORT if available, otherwise default to 3000
+    host: '0.0.0.0', // ensure it binds to all network interfaces
+  },
 })
